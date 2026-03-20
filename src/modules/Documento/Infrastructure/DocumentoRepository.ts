57824978b08c8731d7ca6445da60134e7781faf8
Raniere <i4376@rn.senac.br>
2026-03-20 19:45:12 -0300

    Foi adicionado o repositório (DocumentoRepository.ts) do Domain Documento

---
+++ src/modules/Documento/Infrastructure/DocumentoRepository.ts
@@ -0,0 +1,42 @@
+import { Documento } from "../Domain/Documento";
+
+export class DocumentoRepository{
+
+    private static documentos: Documento[] = [];
+
+
+    // LISTAR DOCUMENTOS:
+    async listarDocumentos(){
+        return DocumentoRepository.documentos;
+    }
+
+
+    // INSERIR DOCUMENTO:
+    async inserirDocumento(documento: Documento){
+        DocumentoRepository.documentos.push(documento);
+    }
+
+
+    // BUSCAR POR ID:
+    async buscarDocumentoPorId(idDocumento: number){
+        return DocumentoRepository.documentos.find(doc => doc.idDocumento === idDocumento);
+    }
+
+    // REMOVER DOCUMENTO:
+    async removerDocumento(idDocumento: number){
+        DocumentoRepository.documentos = DocumentoRepository.documentos.filter(doc => doc.idDocumento !== idDocumento);
+    }
+
+    // ATUALIZAR DOCUMENTO:
+    async atualizarDocumento(documento: Documento){
+
+        // Buscando a posição em que o documento solicitado para autualizar está dentro do array.
+        const indice = DocumentoRepository.documentos.findIndex(doc => doc.idDocumento === documento.idDocumento);
+
+        if(indice !== -1){
+            DocumentoRepository.documentos[indice] = documento;
+        } else{
+            console.log('Documento não encontrado!');
+        }
+    }
+}
